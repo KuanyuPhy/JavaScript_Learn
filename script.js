19624b2757f3bd57eccd3d8c264734b39ac204ce
kuanyuphy <spaul6627@gmail.com>
2023-10-21 12:19:29 +0800

    update var, let console learn

---
+++ script.js
@@ -0,0 +1,13 @@
+// console.log("hello js")
+// console.log("Hi, JS")
+
+// 變數宣告 
+
+let name = "coffee"
+//console.log(name)
+
+let sentence = 'Good morning ~'
+//console.log(name + sentence)
+
+coffee = prompt("which coffee do you want to buy ?")
+console.log(coffee)
